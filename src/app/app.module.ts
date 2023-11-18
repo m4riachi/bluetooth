6d6555b5435ca10cbb1c0d38be44f6d43de88f9b
taoufiq <taoufiq.benmessaoud@gmail.com>
2023-11-18 17:34:07 +0100

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { ItemsComponent } from './item/items.component'
 import { ItemDetailComponent } from './item/item-detail.component'
+import { DropDownModule } from "nativescript-drop-down/angular";
 
 @NgModule({
   bootstrap: [AppComponent],
-  imports: [NativeScriptModule, AppRoutingModule],
+  imports: [NativeScriptModule, AppRoutingModule, DropDownModule],
   declarations: [AppComponent, ItemsComponent, ItemDetailComponent],
   providers: [],
   schemas: [NO_ERRORS_SCHEMA],
--- src/app/item/items.component.ts
+++ src/app/item/items.component.ts
@@ -9,14 +9,19 @@ import permissions from "@master.technology/permissions"
   templateUrl: './items.component.html',
 })
 export class ItemsComponent implements OnInit {
+  items: any
   bluetooth: Bluetooth = null
   enabled: boolean = false
+  selectedIndex: string = 'Sergio'
+  frequence: string = ''
+  adresse: string = ''
 
   constructor(private itemService: ItemService) {
     this.bluetooth = new Bluetooth();
   }
 
   ngOnInit(): void {
+    this.items = this.itemService.getItems().map((item) => item.name);
     permissions.requestPermission(permissions.PERMISSIONS.BLUETOOTH)
       .then( () => {
         console.log("Woo Hoo, I have the power!");
@@ -24,6 +29,11 @@ export class ItemsComponent implements OnInit {
       .catch( () => {
         console.log("Uh oh, no permissions - plan B time!");
       });
+
+    this.bluetooth.isBluetoothEnabled().then((enabled) => {
+        this.enabled = enabled;
+      }
+    );
   }
   onTap() {
     this.bluetooth.isBluetoothEnabled().then((enabled) => {
@@ -33,7 +43,7 @@ export class ItemsComponent implements OnInit {
     console.log("enabled: ");
 
     this.bluetooth.startScanning({
-      filters: [{serviceUUID:'180d'}],
+      //filters: [{serviceUUID:'180d'}],
       seconds: 4,
       onDiscovered: function (peripheral) {
         console.log("Periperhal found with UUID: " + peripheral.UUID);
@@ -45,5 +55,20 @@ export class ItemsComponent implements OnInit {
     }).finally(() => {
       console.log("scanning finished");
     });
+    this.frequence = '66';
+    this.adresse = '12 Rue Michel de Montaigne, 56000 Vannes';
+    this.bluetooth.read({
+      peripheralUUID: '00001101-0000-1000-8000-00805f9b34fb',
+      serviceUUID: '180d',
+      characteristicUUID: '3434-45234-34324-2343'
+    }).then(function(result) {
+      // fi. a heartrate monitor value (Uint8) can be retrieved like this:
+      var data = new Uint8Array(result.value);
+      console.log("Your heartrate is: " + data[1] + " bpm");
+    }, function (err) {
+      console.log("read error: " + err);
+    });
+
+
   }
 }
