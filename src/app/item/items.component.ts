6e5d0f492ab2d89306999f737bb419b7715f1c85
taoufiq <taoufiq.benmessaoud@gmail.com>
2023-11-18 15:17:11 +0100

    commit

--- src/app/item/items.component.ts
+++ src/app/item/items.component.ts
@@ -1,18 +1,49 @@
 import { Component, OnInit } from '@angular/core'
-
-import { Item } from './item'
+import { Bluetooth } from '@nativescript-community/ble';
 import { ItemService } from './item.service'
+import permissions from "@master.technology/permissions"
+
 
 @Component({
   selector: 'ns-items',
   templateUrl: './items.component.html',
 })
 export class ItemsComponent implements OnInit {
-  items: Array<Item>
+  bluetooth: Bluetooth = null
+  enabled: boolean = false
 
-  constructor(private itemService: ItemService) {}
+  constructor(private itemService: ItemService) {
+    this.bluetooth = new Bluetooth();
+  }
 
   ngOnInit(): void {
-    this.items = this.itemService.getItems()
+    permissions.requestPermission(permissions.PERMISSIONS.BLUETOOTH)
+      .then( () => {
+        console.log("Woo Hoo, I have the power!");
+      })
+      .catch( () => {
+        console.log("Uh oh, no permissions - plan B time!");
+      });
+  }
+  onTap() {
+    this.bluetooth.isBluetoothEnabled().then((enabled) => {
+        this.enabled = enabled;
+      }
+    );
+    console.log("enabled: ");
+
+    this.bluetooth.startScanning({
+      filters: [{serviceUUID:'180d'}],
+      seconds: 4,
+      onDiscovered: function (peripheral) {
+        console.log("Periperhal found with UUID: " + peripheral.UUID);
+      }
+    }).then(() => {
+      console.log("scanning complete 1");
+    }).catch((err) => {
+      console.log("error while scanning 2: " + err);
+    }).finally(() => {
+      console.log("scanning finished");
+    });
   }
 }
